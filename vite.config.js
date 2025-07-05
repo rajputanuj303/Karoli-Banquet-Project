625b856def29f7131d44a6b9cf9453680fd2edaf
rajputanuj303 <rajputanuj303@gmial.com>
2025-07-06 01:58:58 +0530

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Karoli-Banquet-Project/'
+  base: '/your-repo-name/', // replace with your actual repo name
   plugins: [react()],
 })
